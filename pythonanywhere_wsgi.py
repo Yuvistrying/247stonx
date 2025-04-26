3ccd0166069b4de99bc7cf97357b58707e006900
Yuvistrying <134134612+Yuvistrying@users.noreply.github.com>
2025-04-26 19:31:56 +0300

    Update UI and deployment files for better user experience

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -2,13 +2,13 @@ import sys
 import os
 
 # Add the application directory to the Python path
-path = '/home/YOURUSERNAME/247stonx'
+path = '/home/yuvalalalalal/247stonx'
 if path not in sys.path:
     sys.path.append(path)
 
 # Set production environment variables
 os.environ['PRODUCTION'] = 'true'
-os.environ['SECRET_KEY'] = 'your-secure-production-key-change-this'  # IMPORTANT: Change this to a secure random key
+os.environ['SECRET_KEY'] = 'af8d1cbc41ba4e91b756c8e9e4c2e3a9f7g6h5j4k3l2m1'  # Random secure key
 
 # Import the Flask application
 from app import app as application 
